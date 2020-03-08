df44947c319c046628a3a28dc74125b571e33372
Clemens Prescher <clemens.prescher@gmail.com>
2020-03-08 12:44:32 +0100

    Load Image works now correctly

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,13 @@
+import io
+
 import flask
 from flask import request
 
+import numpy as np
+import fabio
+
 app = flask.Flask(__name__)
+
 
 class ImgBrowser():
     def __init__(self):
@@ -9,13 +15,20 @@ class ImgBrowser():
         self.img_data = None
 
     def load_image(self, filename):
-        self.img_data = "haha"
+        self.img_data = fabio.open(filename).data
+
+    @property
+    def byte_img(self):
+        bytestream = io.BytesIO()
+        np.save(bytestream, self.img_data)
+        return bytestream.getvalue()
+
 
 browser = ImgBrowser()
 
 
 @app.route('/load', methods=['POST'])
 def load_image():
-    filename = request.args.get('filename')
+    filename = request.form.get('filename')
     browser.load_image(filename)
-    return browser.img_data
+    return browser.byte_img
--- backend/tests/test_flask.py
+++ backend/tests/test_flask.py
@@ -1,7 +1,19 @@
 import os
+import io
 import unittest
+import tempfile
+
+from PIL import Image
+import numpy as np
 
 from backend.app import app
+
+
+def create_random_tiff(img_data):
+    temp_file = tempfile.NamedTemporaryFile("wb", suffix=".tiff")
+    im = Image.fromarray(img_data)
+    im.save(temp_file)
+    return temp_file
 
 
 class BasicTests(unittest.TestCase):
@@ -10,5 +22,12 @@ class BasicTests(unittest.TestCase):
         self.app = app.test_client()
 
     def test_load_image(self):
-        response = self.app.post('/load', data=dict(filename='blubg'))
-        print(response)
+        rand_img = np.random.random((5, 5))
+        temp_file = create_random_tiff(rand_img)
+
+        response = self.app.post('/load', data=dict(filename=temp_file.name))
+        received_img = np.load(io.BytesIO(response.data))
+
+        self.assertTrue(np.allclose(received_img, rand_img))
+        self.assertEqual(rand_img.shape, received_img.shape)
+
